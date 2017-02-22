95617fac142e10deebbc0718232db6b2a5e9cab9
Ravindra Bandara <ravindra.bandara@gmail.com>
2017-02-22 01:07:52 -0500

    work in progress

--- src/LoginControl.js
+++ src/LoginControl.js
@@ -1,71 +1,83 @@
 import React, { Component } from 'react';
 
 class LoginControl extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleLoginClick = this.handleLoginClick.bind(this);
-    this.handleLogoutClick = this.handleLogoutClick.bind(this);
-    this.state = {isLoggedIn: false};
-  }
-
-  handleLoginClick() {
-    this.setState({isLoggedIn: true});
-  }
-
-  handleLogoutClick() {
-    this.setState({isLoggedIn: false});
-  }
-
-  render() {
-    const isLoggedIn = this.state.isLoggedIn;
-    
-    let button = null;
-    if (isLoggedIn) {
-      button = <LogoutButton onClick={this.handleLogoutClick} />;
-    } else {
-      button = <LoginButton onClick={this.handleLoginClick} />;
+    constructor(props) {
+        super(props);
+        this.handleLoginClick = this.handleLoginClick.bind(this);
+        this.handleLogoutClick = this.handleLogoutClick.bind(this);
+        this.state = { isLoggedIn: false };
+        this.state = {rsession:"", token:""};
     }
 
-    return (
-      <div>
-        <Greeting isLoggedIn={isLoggedIn} />
-        {button}
-      </div>
+    handleLoginClick() {
+        this.setState({ isLoggedIn: true });
+    }
+
+    handleLogoutClick() {
+        this.setState({ isLoggedIn: false });
+    }
+
+     handleSubmit(event) {
+        	fetch(`http://jsonplaceholder.typicode.com/posts`)
+            .then(result=>result.json())
+            // .then(items=> {console.log(items[0].userId)})
+            .then(items=> {this.setState({rsession:items[0].userId, token:items[1] })})
+        event.preventDefault();
+    }
+
+    render() {
+        const isLoggedIn = this.state.isLoggedIn;
+
+        let button = null;
+        if (isLoggedIn) {
+            button = <LogoutButton onClick={this.handleLogoutClick} />;
+        } else {
+            button = <LoginButton onClick={this.fetchData} />;
+        }
+
+        return (
+        <form onSubmit={this.handleSubmit}>
+          	<input type="hidden" name="app" value="10003600" />
+            <input type="hidden" name="rsession" value="" />
+            <input type="hidden" name="token" value=""/>
+            <input type="hidden" name="redirectReq" value="true"/>    
+        <input type="submit" value="Submit" />
+      </form>
     );
-  }
+    }
 }
 
 export default LoginControl;
 
 function UserGreeting(props) {
-  return <h1>Welcome back!</h1>;
+    return <h1>Welcome back!</h1>;
 }
 
 function GuestGreeting(props) {
-  return <h1>Please sign up.</h1>;
+    return <h1>Please sign up.</h1>;
 }
 
 function Greeting(props) {
-  const isLoggedIn = props.isLoggedIn;
-  if (isLoggedIn) {
-    return <UserGreeting />;
-  }
-  return <GuestGreeting />;
+    const isLoggedIn = props.isLoggedIn;
+    if (isLoggedIn) {
+        return <UserGreeting />;
+    }
+    return <GuestGreeting />;
 }
 
 function LoginButton(props) {
-  return (
-    <button onClick={props.onClick}>
-      Login
+    return (
+        <button onClick={props.onClick}>
+            Login
     </button>
-  );
+    );
 }
 
 function LogoutButton(props) {
-  return (
-    <button onClick={props.onClick}>
-      Logout
+    return (
+        <button onClick={props.onClick}>
+            Logout
     </button>
-  );
+    );
 }
 
